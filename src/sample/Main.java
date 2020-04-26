0e49b1353852e4c7af99883b3bb8bdd2054968ea
stgalamt <stgalmat@bx.fallmerayer.it>
2020-04-26 14:11:51 +0200

    Added Loading Screen

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,10 +1,22 @@
 package sample;
 
+import javafx.animation.FadeTransition;
 import javafx.application.Application;
+import javafx.concurrent.Task;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class Main extends Application {
     @Override
@@ -17,14 +29,87 @@ public class Main extends Application {
         root.getTabs().add(new Tab("Verlosung", DrawingTab.generate()));
         root.getTabs().add(new Tab("Einstellungen"));
         root.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
+
+
         primaryStage.setTitle("LesePreisUI");
-        Scene main = new Scene(root, 1280, 1000);
-        primaryStage.setScene(main);
+        //Get Screen Height
+        Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
+
+        //Loading Screen
+        StackPane loadingPane = loadingScreen();
+        Scene loading = new Scene(loadingPane, 1280, screenBounds.getHeight()-25);
+        primaryStage.setScene(loading);
         primaryStage.show();
+
         //Link CSS Stylesheet
-        main.getStylesheets().add("stylesheet.css");
+        loading.getStylesheets().add("stylesheet.css");
+
+        //Button
+        Button startBtn = new Button("LesePreisUI starten");
+        startBtn.setId("drawing-button");
+        startBtn.setTranslateY(350);
+
+        //Button FadeIN
+        FadeTransition fadeIn = new FadeTransition(Duration.seconds(4));
+        fadeIn.setNode(startBtn);
+        fadeIn.setFromValue(0);
+        fadeIn.setToValue(1);
+        fadeIn.setAutoReverse(false);
+        fadeIn.setCycleCount(1);
+        fadeIn.play();
+        loadingPane.getChildren().add(startBtn);
+        startBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                //Main Screen
+                Scene main = new Scene(root, 1280, screenBounds.getHeight()-25);
+                main.getStylesheets().add("stylesheet.css");
+                primaryStage.setScene(main);
+            }
+        });
 
     }
+
+    public static ProgressBar bar = new ProgressBar();
+    public StackPane loadingScreen() throws InterruptedException {
+        StackPane loadingPane = new StackPane();
+        loadingPane.setId("loadingPane");
+        //Logo
+        ImageView logo = new ImageView(new Image("ForcePlateLogo.png"));
+        logo.setFitWidth(900);
+        logo.setPreserveRatio(true);
+
+        //Logo FadeIN
+        FadeTransition fadeIn = new FadeTransition(Duration.seconds(3));
+        fadeIn.setNode(logo);
+        fadeIn.setFromValue(0);
+        fadeIn.setToValue(1);
+        fadeIn.setAutoReverse(false);
+        fadeIn.setCycleCount(1);
+        fadeIn.play();
+        loadingPane.getChildren().add(logo);
+
+
+
+        //ProgressBar
+        bar.setProgress(0);
+        bar.setStyle("-fx-accent: rgb(0,125,67)");
+        bar.setId("progressBar");
+        bar.setTranslateY(150);
+        loadingPane.getChildren().add(bar);
+
+
+        Task<Void> sleeper = new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+                return null;
+            }
+        };
+        bar.progressProperty().bind(sleeper.progressProperty());
+
+        return loadingPane;
+    }
+
 
 
     public static void main(String[] args) {
